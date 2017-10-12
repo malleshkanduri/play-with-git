bb70eee515dbd1d973dce8dc7b8eda58159d3897
Kanduri Durga <mallesh.kanduri@gmail.com>
2017-10-12 10:47:45 -0500

    First COmmit

---
+++ First.java
@@ -0,0 +1,5 @@
+public class First {
+    static public void main(String str[]) {
+        System.out.println(" Hello from First....");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,5 @@
+public class hello {
+    static public void main(String str[]) {
+        System.out.println("Hello how are you.");
+    }
+}
